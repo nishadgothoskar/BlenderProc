beb0e8de650f282a9983a9e6b8c42b87b46b8f2a
Nishad Gothoskar <nishadg@mit.edu>
2022-04-25 13:25:59 -0400

    improve interface

--- examples/nishad/main.py
+++ examples/nishad/main.py
@@ -4,34 +4,30 @@ import os
 import numpy as np
 
 parser = argparse.ArgumentParser()
+parser.add_argument('scene_description_npz', nargs='?', help="Path to scene description")
 parser.add_argument('cc_textures_path', nargs='?', default="resources/cctextures", help="Path to downloaded cc textures")
 parser.add_argument('output_dir', nargs='?', help="Path to where the final files will be saved ")
 args = parser.parse_args()
 
-d = np.load("/home/nishadg/mit/CoraAgent/scene.npy.npz")
+d = np.load(args.scene_description_npz)
 K = d["K"]
-poses = d["poses"]
-cam_pose = d["camera"]
+poses = d["object_poses"]
+cam_pose = d["camera_pose"]
 box_dims = d["box_dims"]
 im_width, im_height = d["image_dims"]
-print(K)
-print(box_dims)
-print(box_dims.shape)
-print(poses.shape)
-num_objects = poses.shape[0]
+floor_material = d["floor_material"]
+wall_material = d["wall_material"]
+object_materials = d["object_materials"]
+(room_x_half_width, room_z_half_width) = d["room_half_widths"]
+floor_y = d["floor_y"]
 
-print(poses[0,:,:])
-print(poses[1,:,:])
-print(poses[2,:,:])
-print(poses[3,:,:])
-print(poses[4,:,:])
+num_objects = poses.shape[0]
 
 bproc.init()
 objects = [
     bproc.object.create_primitive("CUBE", scale=[box_dims[i,0]/2,box_dims[i,1]/2,box_dims[i,2]/2])
     for i in range(num_objects)
 ]
-print(len(objects))
 
 # set shading and physics properties and randomize PBR materials
 for j, obj in enumerate(objects):
@@ -42,13 +38,14 @@ for j, obj in enumerate(objects):
     )
         
 floor_plane = [
-    bproc.object.create_primitive('PLANE', scale=[2, 2, 1], location=[0, 0.5 - 0.025, 0], rotation=[np.pi/2, 0, 0]),
+    bproc.object.create_primitive('PLANE',
+    scale=[room_x_half_width*2, room_z_half_width*2, 1], location=[0, floor_y, 0], rotation=[np.pi/2, 0, 0]),
 ]
 wall_planes = [
-    bproc.object.create_primitive('PLANE', scale=[2, 2, 1], location=[0, 0, -2], rotation=[0, 0, 0]),
-    bproc.object.create_primitive('PLANE', scale=[2, 2, 1], location=[2, 0, 0], rotation=[0, np.pi/2, 0]),
-    bproc.object.create_primitive('PLANE', scale=[2, 2, 1], location=[0, 0, 2], rotation=[0, 0, 0]),
-    bproc.object.create_primitive('PLANE', scale=[2, 2, 1], location=[-2, 0, 0], rotation=[0, np.pi/2, 0]),
+    bproc.object.create_primitive('PLANE', scale=[room_x_half_width*2, 2, 1], location=[0, 0, -room_z_half_width], rotation=[0, 0, 0]),
+    bproc.object.create_primitive('PLANE', scale=[2, room_z_half_width*2, 1], location=[room_x_half_width, 0, 0], rotation=[0, np.pi/2, 0]),
+    bproc.object.create_primitive('PLANE', scale=[room_x_half_width*2, 2, 1], location=[0, 0, room_z_half_width], rotation=[0, 0, 0]),
+    bproc.object.create_primitive('PLANE', scale=[2, room_z_half_width*2, 1], location=[-room_x_half_width, 0, 0], rotation=[0, np.pi/2, 0]),
 ]
 for plane in (floor_plane + wall_planes):
     plane.enable_rigidbody(False, collision_shape='BOX', friction = 100.0, linear_damping = 0.99, angular_damping = 0.99)
@@ -72,37 +69,27 @@ light_point.set_location([0.0, -1.0, 0.0])
 # sample CC Texture and assign to room planes
 cc_textures = bproc.loader.load_ccmaterials(args.cc_textures_path)
 print([c.get_attr("name") for c in cc_textures])
-texture_name = "Wood081"
+texture_name = floor_material
 tex = bproc.filter.one_by_attr(cc_textures, "name", texture_name)
 for plane in floor_plane:
     plane.replace_materials(tex)
 
-texture_name = "Plaster001"
+texture_name = wall_material
 tex = bproc.filter.one_by_attr(cc_textures, "name", texture_name)
 for plane in wall_planes:
     plane.replace_materials(tex)
 
-texture_names = ["Metal030" for _ in range(len(objects))] 
+texture_names = object_materials
 for (i, obj) in enumerate(objects):
     tex = bproc.filter.one_by_attr(cc_textures, "name", texture_names[i])
     obj.replace_materials(tex)
 
-
-# # Define a function that samples 6-DoF poses
-# def sample_pose_func(obj: bproc.types.MeshObject):
-#     min = np.random.uniform([-0.3, -0.3, 0.0], [-0.2, -0.2, 0.0])
-#     max = np.random.uniform([0.2, 0.2, 0.4], [0.3, 0.3, 0.6])
-#     obj.set_location(np.random.uniform(min, max))
-#     obj.set_rotation_euler(bproc.sampler.uniformSO3())
-
 # Set intrinsics via K matrix
 bproc.camera.set_intrinsics_from_K_matrix(K,im_width, im_height)
-
 
 # Change coordinate frame of transformation matrix from OpenCV to Blender coordinates
 cam2world = bproc.math.change_source_coordinate_frame_of_transformation_matrix(cam_pose, ["X", "-Y", "-Z"])
 bproc.camera.add_camera_pose(cam2world)
-
 
 # activate depth rendering
 bproc.renderer.enable_depth_output(activate_antialiasing=False)
@@ -112,11 +99,3 @@ bproc.renderer.set_max_amount_of_samples(50)
 data = bproc.renderer.render()
 
 bproc.writer.write_hdf5(args.output_dir, data)
-
-import h5py
-import os
-
-f = h5py.File(
-    os.path.join('/home/nishadg/mit/BlenderProc/examples/nishad/output/0.hdf5'),
-    'r'
-)
